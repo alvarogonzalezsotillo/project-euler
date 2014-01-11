0c370b395cdfb01fae05a428d1d14d52a9ae167d
alvaro <alvarogonzalezsotillo@gmail.com>
2014-01-11 15:26:29 +0100

    Problema 1 de jaime sin acabar

---
+++ src/main/java/Problema1.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: alvaro
+ * Date: 11/01/14
+ * Time: 14:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class Problema1 {
+
+    public static void main (String [] arg) {
+
+        for( int veces = 1 ; veces < 1000 ; veces++ ){
+            if (veces % 5 == 0 || veces % 3 == 0 )
+        }
+
+    }
+}
