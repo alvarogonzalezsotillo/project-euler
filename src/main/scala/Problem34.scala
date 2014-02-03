edcec3c40ff3f7a1156cc56fad9e32a160ac09ca
Sergio de Mingo <sdemingo@gmail.com>
2014-02-03 20:03:08 +0100

    n 34

--- src/main/scala/Problem34.scala
+++ src/main/scala/Problem34.scala
@@ -11,4 +11,15 @@ Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 
 */
 
+  def fact( n: Int ) : Int = if( n <= 1 ) 1 else n*fact(n-1)
+  val factorial = (0 to 9).map( n => (n,fact(n))).toMap
+  def digits(n :Int) = n.toString.map( _ - '0')
+  def curious( n: Int ) = digits(n).map(factorial).sum == n
+  
+  val max = 100000000
+  
+  val solution = (1 to max).filter(curious)
+  
+  println( s"Solution:${solution.sum}  \n${solution.mkString("\n")}" )
+
 }
