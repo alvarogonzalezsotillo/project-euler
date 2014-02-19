eda1da1ee7fd35985d4d6e18196293c66240a1f3
unknown <alvaro@aula23_pcprofe.aula23.local>
2014-02-19 19:44:52 +0100

    hormigas maybe solved

--- src/main/java/Hormigas.java
+++ src/main/java/Hormigas.java
@@ -28,90 +28,21 @@ class Main
 		}
 	}
 
-	private static int solucionaMaximo_lento(int longitud, int hormigas[])
+	private static int solucionaMaximo(int longitud, int hormigas[])
 	{
-		double mitad = 1. * longitud / 2;
-		int hormigasSimuladas[] = new int[hormigas.length];
-		System.arraycopy(hormigas, 0, hormigasSimuladas, 0, hormigas.length);
-		int sentidoHormigas[] = new int[hormigas.length];
-
+		double distanciaMasGrandeAExtremo = 0;
 		for (int h = 0; h < hormigas.length; h++)
 		{
-			if (hormigasSimuladas[h] < mitad)
-				sentidoHormigas[h] = 1;
-			else
-				sentidoHormigas[h] = -1;
+			distanciaMasGrandeAExtremo = Math.max(distanciaMasGrandeAExtremo, hormigas[h]);
+			distanciaMasGrandeAExtremo = Math.max(distanciaMasGrandeAExtremo, longitud - hormigas[h]);
 		}
-
-		boolean hormigaEnElPalo = true;
-		int pasos = 0;
-		while (hormigaEnElPalo)
-		{
-			pasos++;
-			int nextHormigasSimuladas[] = new int[hormigasSimuladas.length];
-
-			for (int h = 0; h < hormigasSimuladas.length; h++)
-			{
-				nextHormigasSimuladas[h] = hormigasSimuladas[h] + sentidoHormigas[h];
-			}
-
-			for (int h1 = 0; h1 < hormigasSimuladas.length; h1++)
-			{
-				for (int h2 = h1 + 1; h2 < hormigasSimuladas.length; h2++)
-				{
-					boolean cruce = (nextHormigasSimuladas[h1] < nextHormigasSimuladas[h2] &&
-									hormigasSimuladas[h1] > nextHormigasSimuladas[h2]) ||
-
-									(nextHormigasSimuladas[h1] > nextHormigasSimuladas[h2] &&
-									hormigasSimuladas[h1] < nextHormigasSimuladas[h2]);
-
-
-
-					boolean toque = nextHormigasSimuladas[h1] == nextHormigasSimuladas[h2];
-
-					if (cruce)
-					{
-						nextHormigasSimuladas[h1] = hormigasSimuladas[h1];
-						nextHormigasSimuladas[h2] = hormigasSimuladas[h2];
-						hormigasSimuladas[h1] *= -1;
-						hormigasSimuladas[h2] *= -1;
-					}
-					else if (toque)
-					{
-						hormigasSimuladas[h1] *= -1;
-						hormigasSimuladas[h2] *= -1;
-					}
-				}
-			}
-
-			System.arraycopy(nextHormigasSimuladas, 0, hormigasSimuladas, 0, hormigasSimuladas.length);
-
-			hormigaEnElPalo = false;
-			for (int h = 0; h < hormigasSimuladas.length; h++)
-			{
-				if (hormigasSimuladas[h] > 0 && hormigasSimuladas[h] < longitud)
-				{
-					hormigaEnElPalo = true;
-				}
-			}
-			
-			{
-				System.err.print("Paso " + pasos + "  --> " );
-				for (int h = 0; h < hormigasSimuladas.length; h++)
-				{
-					System.err.print(hormigasSimuladas[h] + "  ");
-				}
-				System.err.println();
-			}
-		}
-
-		return pasos;
+		return (int)distanciaMasGrandeAExtremo;
 	}
 
 	private static void soluciona(int longitud, int hormigas[])
 	{
 		int minimo = solucionaMinimo(longitud, hormigas);
-		int maximo = solucionaMaximo_lento(longitud, hormigas);
+		int maximo = solucionaMaximo(longitud, hormigas);
 		System.out.println(minimo + " " + maximo);
 	}
 
