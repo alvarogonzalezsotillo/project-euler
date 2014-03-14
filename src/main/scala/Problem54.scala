0a9c7b8b98f1cb5dd36919023a7f55b36a6b1763
alvaro <alvarogonzalezsotillo@gmail.com>
2014-03-14 11:45:03 +0100

    need to parse the poker data file

--- src/main/scala/Problem54.scala
+++ src/main/scala/Problem54.scala
@@ -1,3 +1,4 @@
+
 /*
 n the card game poker, a hand consists of five cards and are ranked, from lowest to highest, in the following way:
 
@@ -14,7 +15,10 @@ Royal Flush: Ten, Jack, Queen, King, Ace, in same suit.
 The cards are valued in the order:
 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King, Ace.
 
-If two players have the same ranked hands then the rank made up of the highest value wins; for example, a pair of eights beats a pair of fives (see example 1 below). But if two ranks tie, for example, both players have a pair of queens, then highest cards in each hand are compared (see example 4 below); if the highest cards tie then the next highest cards are compared, and so on.
+If two players have the same ranked hands then the rank made up of the highest value wins; for example, a pair of eights
+beats a pair of fives (see example 1 below). But if two ranks tie, for example,
+both players have a pair of queens, then highest cards in each hand are compared (see example 4 below);
+if the highest cards tie then the next highest cards are compared, and so on.
 
 Consider the following five hands dealt to two players:
 
@@ -24,16 +28,19 @@ Pair of Fives
  	2C 3S 8S 8D TD
 Pair of Eights
  	Player 2
+
 2	 	5D 8C 9S JS AC
 Highest card Ace
  	2C 5C 7D 8S QH
 Highest card Queen
  	Player 1
+
 3	 	2D 9C AS AH AC
 Three Aces
  	3D 6D 7D TD QD
 Flush with Diamonds
  	Player 2
+
 4	 	4D 6S 9H QH QC
 Pair of Queens
 Highest card Nine
@@ -53,51 +60,157 @@ The file, poker.txt, contains one-thousand random hands dealt to two players. Ea
 How many hands does Player 1 win?
 */
 
-object Problem54 extends App{
+object Problem54 extends App {
 
   type Suit = Char
   type Rank = Int
-  
-  object Rank{
-    val fromCharMap = Map( 'T' -> "10", 'J' -> "11", 'Q' ->"12" , 'K' ->"13", 'A'->"14").withDefault( c => "0" + c )
-    def apply( c: Char ) = fromCharMap(c)
+
+  object Rank {
+    val fromCharMap = Map('T' -> 10, 'J' -> 11, 'Q' -> 12, 'K' -> 13, 'A' -> 14).withDefault(c => c - '0')
+
+    def valid(rank: Rank) = rank >= 2 && rank <= 14
+
+    def apply(c: Char) = fromCharMap(c) ensuring valid _
+
+    def apply(i: Int) = i ensuring valid _
+
+    def toString(rank: Rank) = {
+      assert(valid(rank))
+      String.format("%02d", rank: Integer)
+    }
   }
-  
-  class Card( val rank: Rank, val suit: Suit ) extends Pair(rank,suit)
-  object Card{
-    def apply( s: String ) = new Card( Rank(s(0)), s(1) )
+
+  class Card(val rank: Rank, val suit: Suit) extends Pair(rank, suit)
+
+  object Card {
+    def apply(s: String) = new Card(Rank(s(0)), s(1))
   }
-  
-  class Hand( cards: Seq[Card] ){
-    val _cards = cards.toArray.sortBy( _.rank )
-    
-    def hasRank( rank: Rank ) = ! _cards.find( _.rank == rank ).isEmpty
-    
-    def royalFlush() : Option[String]= {
-      straightFlush match{
-        case Some(10) => String "Arf14"
+
+  class Hand(cards: Seq[Card]) extends Ordered[Hand] {
+    val _cards = cards.toArray.sortBy(_.rank)
+
+    lazy val kinds = _cards.groupBy(_.rank).map {
+      case (rank, cards) => (cards.size, rank)
+    }.toArray.sortBy {
+      case (size, rank) => rank
+    }
+
+    type Match = Option[Seq[Rank]]
+
+    def royalFlush(): Match = {
+      straightFlush match {
+        case Some(Seq(10)) => Some(Seq(14))
         case _ => None
       }
     }
-    
-    def straightFlush : Option[Rank]= {
+
+    def straightFlush: Match = {
       val first = _cards.head
-      val yes = _cards.tail.forall( _.suit == first.suit ) && (1 to 4).forall( i => _cards(i).rank == first.rank + i )
-      if( yes )
-        Some(first.rank)
-      else
+      flush.isDefined && straight.isDefined match {
+        case true => Some(Seq(first.rank))
+        case _ => None
+      }
+    }
+
+    def straight: Match = {
+      val first = _cards.head
+      (1 to 4).forall(i => _cards(i).rank == first.rank + i) match {
+        case true => Some(Seq(first.rank))
+        case _ => None
+      }
+    }
+
+    def findOfAKind(howMany: Int) = kinds.filter {
+      case (size, _) => size == howMany
+    }
+
+    def someOfAKind(howMany: Int): Match = {
+      val found = findOfAKind(howMany)
+      if (found.size == 0)
         None
+      else
+        Some(found.map(_._2).sorted.reverse)
     }
-    
-    def fourOfAKind : Option[Rank] = {
-      ( 1 to 14 ).find( r => _cards.count( _.rank == r) == 4 )
+
+    def fourOfAKind = someOfAKind(4)
+
+    def fullHouse: Match = {
+      for (t3 <- threeOfAKind; t2 <- pair) yield t3 ++ t2
     }
-    
-    
+
+    def threeOfAKind = someOfAKind(3)
+
+    def twoPairs = {
+      val found = someOfAKind(2)
+      if (found.isEmpty || found.get.size < 2)
+        None
+      else
+        found
+    }
+
+    def pair = someOfAKind(2)
+
+    def highest: Match = Some(_cards.reverse.map(_.rank))
+
+    def flush = {
+      _cards.forall(_.suit == _cards.head.suit) match {
+        case true => highest
+        case _ => None
+      }
+    }
+
+    def describe = {
+      val matches = Seq(royalFlush, straightFlush, fourOfAKind, fullHouse, flush, straight, threeOfAKind, twoPairs, pair, highest)
+
+      matches.zipWithIndex.foldLeft("")((ret, m) => m match {
+        case (Some(s), i) => ret + " " + ('Z' - i).toChar + s.foldLeft("")((str, r) => str + Rank.toString(r))
+        case _ => ret
+      })
+    }
+
+    def compare(that: Hand): Int = describe compare that.describe
+
+    override def toString = describe
   }
-  object Hand{
-    def apply( s: String ) = new Hand(s.split( " " ).map(Card.apply))
+
+  object Hand {
+    def apply(s: String) = new Hand(s.split(" ").map(Card.apply))
   }
-  
-  
+
+
+  val samples = Seq(
+    ("5H 5C 6S 7S KD", "2C 3S 8S 8D TD", true),
+    ("5D 8C 9S JS AC", "2C 5C 7D 8S QH", false),
+    ("2D 9C AS AH AC", "3D 6D 7D TD QD", true),
+    ("4D 6S 9H QH QC", "3D 6D 7H QD QS", false),
+    ("2H 2D 4C 4D 4S", "3C 3D 3S 9S 9D", false)
+  )
+
+  /*
+
+
+  4
+    Pair of Queens
+  Highest card Nine
+
+    Pair of Queens
+  Highest card Seven
+  Player 1
+
+
+  5
+  Full House
+    With Three Fours
+
+  Full House
+  with Three Threes
+    Player 1
+
+  */
+
+  for ((s1, s2, res) <- samples) {
+    val h1 = Hand(s1)
+    val h2 = Hand(s2)
+    println(s"$s1-->$h1    $s2-->$h2     ${h1 < h2 == res}")
+  }
 }
