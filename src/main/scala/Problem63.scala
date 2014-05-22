cbf75c324f5909dfc82540314dab192a6d5206b6
unknown <alvaro@aula23_pcprofe.aula23.local>
2014-05-22 18:15:12 +0200

    63 solved

--- src/main/scala/Problem63.scala
+++ src/main/scala/Problem63.scala
@@ -5,7 +5,16 @@ How many n-digit positive integers exist which are also an nth power?
 */
 object Problem63 extends App{
 
-  type Numero = Long
+  def measure[T]( msg: String = "" )( proc: =>T ) = {
+    println( s"-->$msg" )
+    val ini = System.currentTimeMillis
+    val ret = proc
+    val end = System.currentTimeMillis
+    println( s"<--$msg: ${end-ini} ms" )
+    ret
+  }
+
+  type Numero = BigInt
   
   implicit class NumeroOps( n: Numero ){
     def **(e: Numero) : Numero = if( e == 0) 1 else n * ( n**(e-1) )
@@ -14,12 +23,26 @@ object Problem63 extends App{
   
   def basesFor( n: Numero ) = {
     Iterator.from(1).
-             map(_.toLong).
+             map( BigInt(_) ).
              dropWhile( i => (i**n).digitsSize < n ).
              takeWhile( i => (i**n).digitsSize == n ).
              toList
   }
-  
-  val s = (1 to 30).map( n => basesFor(n) )
-  println( s.zipWithIndex.mkString("\n") )
+
+  measure(){
+    val s = Iterator.from(1).
+                     map( n => basesFor(n) ).
+                     takeWhile( _.size > 0 ).
+                     toList
+                     
+    s.zipWithIndex.foreach{ b => 
+      val exp = b._2+1
+      println( b )
+      b._1.foreach( n => println( s"$n^$exp = ${n**exp}" ) )
+    }
+    
+    println( s )
+    val solution = s.flatten.size
+    println( s"Solution:$solution" )
+  }
 }
